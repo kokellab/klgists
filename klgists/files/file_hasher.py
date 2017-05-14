0f01d9f19645a54e57b83b4832cb86c0080c2c8e
Douglas Myers-Turnbull <dmyersturnbull@gmail.com>
2017-05-13 19:54:23 -0700

    Fixed a bug in Hasher.

--- klgists/files/file_hasher.py
+++ klgists/files/file_hasher.py
@@ -1,8 +1,11 @@
 # Adapted from the Gist: https://gist.github.com/dmyersturnbull/f0116c52feae3094f66b0c99b586d166
 
+from typing import Callable, Any
 import hashlib
 import os
+import codecs
 import gzip
+
 
 class FileHasher:
 
@@ -10,20 +13,22 @@ class FileHasher:
 	extension = None
 	buffer_size = None
 
-	def __init__(self, algorithm=hashlib.sha1, extension: str='.sha1', buffer_size: int = 16*1024):
+	def __init__(self, algorithm: Callable[[], Any]=hashlib.sha1, extension: str='.sha1', buffer_size: int = 16*1024):
 		self.algorithm = algorithm
 		self.extension = extension
 		self.buffer_size = buffer_size
 
 	def hashsum(self, file_name: str) -> str:
+		alg = self.algorithm()
 		with open(file_name, 'rb') as f:
-			for chunk in iter(lambda: f.read(4096), b''):
-				self.algorithm.update(chunk)
-		return self.algorithm.hexdigest()
+			for chunk in iter(lambda: f.read(self.buffer_size), b''):
+				alg.update(chunk)
+		return alg.hexdigest()
 
 	def add_hash(self, file_name: str) -> None:
 		with open(file_name + self.extension, 'w') as f:
-			f.write(self.hashsum(file_name))
+			s = self.hashsum(file_name)
+			f.write(s)
 
 	def check_hash(self, file_name: str) -> bool:
 		if not os.path.isfile(file_name + self.extension): return False
@@ -31,7 +36,7 @@ class FileHasher:
 			return f.read() == self.hashsum(file_name)
 
 	def check_and_open(self, file_name: str, *args):
-		return self._o(file_name, opener=open, *args)
+		return self._o(file_name, opener=lambda f: codecs.open(f, encoding='utf-8'), *args)
 
 	def check_and_open_gzip(self, file_name: str, *args):
 		return self._o(file_name, opener=gzip.open, *args)
