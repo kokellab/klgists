48a0d9786ed2c586e339866dab193d54341cfba8
cole helsell <cole.helsell@gmail.com>
2017-05-14 12:39:22 -0700

    Minor: nicer notifications.

--- klgists/misc/colored_notifications.py
+++ klgists/misc/colored_notifications.py
@@ -4,23 +4,23 @@ from colorama import Fore, Style
 
 def warn_user(*lines: str):
 	print()
-	_print_to_user(['', *lines, ''], Fore.RED, top='_', bottom='_', sides='|')
+	print_to_user(['', *lines, ''], Fore.RED)
 	print()
 
 def notify_user(*lines: str):
 	print()
-	_print_to_user(['', *lines, ''], Fore.BLUE, top='*', bottom='*', sides='*')
+	print_to_user(['', *lines, ''], Fore.BLUE)
 	print()
 
 def notify_user_light(*lines: str):
 	print()
-	_print_to_user(['', *lines, ''], Style.BRIGHT, top='_', bottom='_', sides='')
+	print_to_user(['', *lines, ''], Style.BRIGHT, sides='')
 	print()
 
 def header_to_user(*lines: str):
-	_print_to_user(lines, Style.BRIGHT, top='', bottom='_', sides='')
+	print_to_user(lines, Style.BRIGHT)
 
-def _print_to_user(lines: Iterable[str], color: int, top: str, bottom: str, sides: str, line_length: int=100):
+def print_to_user(lines: Iterable[str], color: int, top: str='_', bottom: str='_', sides: str='', line_length: int=100):
 	def cl(text: str): print(str(color) + sides + text.center(line_length - 2*len(sides)) + sides)
 	print(str(color) + top * line_length)
 	for line in lines:
