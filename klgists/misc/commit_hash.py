f4afb2ce1007d93989b51d0dd87a5ba69d7ec694
Douglas Myers-Turnbull <dmyersturnbull@gmail.com>
2017-05-26 09:51:12 -0700

    Wrote git_commit_hash

---
+++ klgists/misc/commit_hash.py
@@ -0,0 +1,9 @@
+from subprocess import Popen, PIPE
+
+def git_commit_hash(git_repo_dir: str='.') -> str:
+  """Gets the hex of the most recent Git commit hash in git_repo_dir."""
+	p = Popen(['git', 'rev-parse', 'HEAD'], stdout=PIPE, cwd=git_repo_dir)
+	(out, err) = p.communicate()
+	exit_code = p.wait()
+	if exit_code != 0: raise ValueError("Got nonzero exit code {} from git rev-parse".format(exit_code))
+	return out.decode('utf-8').rstrip()
