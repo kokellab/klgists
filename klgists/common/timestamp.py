3b19c5dc7367a7ae8eb0d171dd52785184430940
Douglas Myers-Turnbull <dmyersturnbull@gmail.com>
2017-06-03 16:12:44 -0700

    Two small new time functions.

--- klgists/common/timestamp.py
+++ klgists/common/timestamp.py
@@ -3,13 +3,25 @@
 import datetime
 
 def format_time(time: datetime.datetime) -> str:
-    """Standard timestamp format. Ex: 2016-05-02_22_35_56."""
-    return time.strftime("%Y-%m-%d_%H-%M-%S")
+	"""Standard timestamp format. Ex: 2016-05-02_22_35_56."""
+	return time.strftime("%Y-%m-%d_%H-%M-%S")
 
 def timestamp() -> str:
-    """Standard timestamp of time now. Ex: 2016-05-02_22_35_56."""
-    return format_time(datetime.datetime.now())
+	"""Standard timestamp of time now. Ex: 2016-05-02_22_35_56."""
+	return format_time(datetime.datetime.now())
 
 def timestamp_path(path: str) -> str:
-    """Standard way to label a file path with a timestamp."""
-    return "{}-{}".format(path, timestamp())
+	"""Standard way to label a file path with a timestamp."""
+	return "{}-{}".format(path, timestamp())
+
+
+def nice_time(n_ms: int) -> str:
+	length = datetime.datetime(1, 1, 1) + datetime.timedelta(milliseconds=n_ms)
+	if n_ms < 1000 * 60 * 60 * 24:
+		return "{}h, {}m, {}s".format(length.hour, length.minute, length.second)
+	else:
+		return "{}d, {}h, {}m, {}s".format(length.day, length.hour, length.minute, length.second)
+
+
+def parse_local_iso_datetime(z: str) -> datetime.datetime:
+	return datetime.datetime.strptime(z, '%Y-%m-%dT%H:%M:%S.%f')
