7388e23718dfa05894b98481f11d2b7593564eb0
UCSFSauron02 <>
2017-06-27 15:19:17 -0700

    Multi-line replace.

--- klgists/files/replace_in_file.py
+++ klgists/files/replace_in_file.py
@@ -1,10 +1,9 @@
 import re
 from typing import Dict
 
-def replace_in_file(self, path: str, changes: Dict[str, str]) -> None:
+def replace_in_file(path: str, changes: Dict[str, str]) -> None:
 	"""Uses re.sub repeatedly to modify (AND REPLACE) a file's content."""
 	with open(path) as f: data = f.read()
 	for key, value in changes.items():
-		data = re.sub(key, value, data)
+		data = re.sub(key, value, data, re.MULTILINE, re.DOTALL)
 	with open(path, 'w') as f: f.write(data)
-
