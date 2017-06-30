6730e0b39281e1a85a0eaa7be62a061a0ea91a1a
Douglas Myers-Turnbull <dmyersturnbull@gmail.com>
2017-06-29 20:40:12 -0700

    Put yield in finally block of silenced.

--- klgists/common/silenced.py
+++ klgists/common/silenced.py
@@ -12,14 +12,16 @@ def silenced(no_stdout=True, no_stderr=True):
     Modified from post by Alex Martelli in https://stackoverflow.com/questions/2828953/silence-the-stdout-of-a-function-in-python-without-trashing-sys-stdout-and-resto/2829036#2829036
     which is licensed under CC BY-SA 3.0 https://creativecommons.org/licenses/by-sa/3.0/
     """
-    if no_stdout:
-        save_stdout = sys.stdout
-        sys.stdout = StringIO()
-    if no_stderr:
-        save_stderr = sys.stderr
-        sys.stderr = StringIO()
-    yield
-    if no_stdout:
-        sys.stdout = save_stdout
-    if no_stderr:
-        sys.stderr = save_stderr
+    try:
+        if no_stdout:
+            save_stdout = sys.stdout
+            sys.stdout = StringIO()
+        if no_stderr:
+            save_stderr = sys.stderr
+            sys.stderr = StringIO()
+        yield
+    finally:
+        if no_stdout:
+            sys.stdout = save_stdout
+        if no_stderr:
+            sys.stderr = save_stderr
