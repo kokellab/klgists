b20e6b84d5fccd7bb990ef62e58abc266a65121b
Cole Helsell <cole.helsell@gmail.com>
2017-09-10 20:26:52 -0700

    moved show_df_head to better location

--- klgists/__init__.py
+++
@@ -1,8 +0,0 @@
-
-from IPython.display import display, Markdown
-import pandas as pd
-
-def df_head(df: pd.DataFrame, n_rows:int=1) -> None:
-	"""Pretty-print the head of a Pandas table in a Jupyter notebook and show its dimensions."""
-	display(Markdown("**whole table (below):** {} rows × {} columns".format(len(df), len(df.columns))))
-	display(df.head(n_rows))
