7387d852c9674704d56c9990292759f094775b31
Douglas Myers-Turnbull <dmyersturnbull@gmail.com>
2017-12-14 14:41:20 -0800

    Don't cfirst if empty.

--- klgists/pandas/__init__.py
+++ klgists/pandas/__init__.py
@@ -21,7 +21,10 @@ def _set_column_sequence(dataframe: pd.DataFrame, col_seq: List[str]) -> pd.Data
 	"""Moves some columns of a Pandas dataframe to the front, returning a copy.
 	Returns: a copy of the dataframe with col_seq as the first columns
 	"""
-	return dataframe[col_seq + [c for c in dataframe.columns if c not in col_seq]]
+	if len(dataframe) == 0:  # will break otherwise
+		return dataframe
+	else:
+		return dataframe[col_seq + [c for c in dataframe.columns if c not in col_seq]]
 
 def cfirst(df: pd.DataFrame, cols: Union[str, int, List[str]]) -> pd.DataFrame:
 	"""Moves some columns of a Pandas dataframe to the front, returning a copy.
