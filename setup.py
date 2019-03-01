ef71fdbee19861933e124c6a1b935ae802fc45ce
Douglas Myers-Turnbull <dmyersturnbull@gmail.com>
2019-02-28 18:29:22 -0800

    Advanced to 0.1.2.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 
 setup(
 	name='klgists',
-	version='0.1.1',
+	version='0.1.2',
 	description='A collection of Python snippets for the Kokel Lab',
 	author='Douglas Myers-Turnbull',
 	url='https://github.com/kokellab/klgists',
