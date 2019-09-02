01b9f36a133fd6ce3143e9d70ccb799b99d705dd
Douglas Myers-Turnbull <dmyersturnbull@gmail.com>
2019-09-02 14:59:28 -0700

    New operators and notifications.

--- klgists/common/operators.py
+++ klgists/common/operators.py
@@ -1,37 +1,94 @@
-# Infix operators like 5 <<isa>> int
-
-from infix import shift_infix, mod_infix, sub_infix
-
-@shift_infix
-def approxeq(a, b):
-	"""This takes 1e-09 * max(abs(a), abs(b)), which may not be appropriate."""
-	"""Example: 5 <<approxeq>> 5.000000000000001"""
-	return abs(a - b) < 1e-09 * max(abs(a), abs(b))
-
-@shift_infix
-def isa(x, cls): return x.__class__ == cls
+"""
+Operators used for convenience. Although they increase readability, their use can't be refeactored out automatically.
+Carefully decide whether you want to use these.
+"""
+from typing import Any, Optional
+from infix import shift_infix, mod_infix, or_infix
 
 
-@sub_infix
-def o(a, b):
-	"""Usage:
-	print({'a': 1, 'b': 2, 'c': 3} -o- 'c')
-	print({'a': 1, 'b': 2, 'c': 3} -o- {'b', 'c'})
+@or_infix
+def oelse(opt: Optional[Any], fallback: Any) -> Any:
 	"""
-	return {k: v for k, v in a.items() if k not in b}
+	I'm tired of writing `x if x is None else 5`.
+	Now it's just `x |o| 5`
+	"""
+	return fallback if opt is None else opt
 
+@or_infix
+def otry(x, func) -> Any:
+	"""
+	I'm tired of wrapping in try and returning None
+	Now it's just `x |otry| func`
+	Only catches Exception and its subclasses (ex stack overflows will bubble up).
+	"""
+	try:
+		return func(x)
+	except Exception:
+		return None
+
+@or_infix
+def omap(x, func) -> Any:
+	"""
+	I'm tired of writing `try_this(x) if x is None else 5`.
+	Now it's just `x |omap| func`
+	"""
+	return None if x is None else func(x)
+
+@or_infix
+def oadd(x, y) -> Any:
+	"""
+	Add two numbers or return None if either is None.
+	"""
+	return None if x is None or y is None else x + y
+
+@or_infix
+def osub(x, y) -> Any:
+	"""
+	Subtract two numbers or return None if either is None.
+	"""
+	return None if x is None or y is None else x - y
+
+@or_infix
+def omult(x, y) -> Any:
+	"""
+	Multiply two numbers or return None if either is None.
+	"""
+	return None if x is None or y is None else x * y
+
+@or_infix
+def odiv(x, y) -> Any:
+	"""
+	Divide two numbers or return None if either is None.
+	"""
+	return None if x is None or y is None else x / y
+
+@or_infix
+def omod(x, y) -> Any:
+	"""
+	Modulo two numbers or return None if either is None.
+	"""
+	return None if x is None or y is None else x % y
+
+@shift_infix
+def isa(x, cls):
+	"""
+	Example: 5 %isa% int
+	"""
+	return isinstance(x, cls)
 
 @mod_infix
 def eq(a, b):
-	"""Approximately equal. This takes 1e-09 * max(abs(a), abs(b)), which may not be appropriate."""
-	"""Example: 5 %eq% 5.000000000000001"""
+	"""
+	Approximately equal. This takes 1e-09 * max(abs(a), abs(b)), which may not be appropriate.
+	Example: 5 %eq% 5.000000000000001"
+	"""
 	return abs(a - b) < 1e-09 * max(abs(a), abs(b))
 
 @mod_infix
 def neq(a, b):
-	"""Not approximately equal. This takes 1e-09 * max(abs(a), abs(b)), which may not be appropriate."""
-	"""Example: 5 %eq% 5.000000000000001"""
+	"""
+	Not approximately equal. This takes 1e-09 * max(abs(a), abs(b)), which may not be appropriate.
+	Example: 5 %neq% 5.000000000000001"
+	"""
 	return abs(a - b) >= 1e-09 * max(abs(a), abs(b))
 
-
-__all__ = ['approxeq', 'isa', 'o', 'eq', 'neq']
--- klgists/misc/colored_notifications.py
+++ klgists/misc/colored_notifications.py
@@ -1,53 +1,58 @@
-from typing import Iterable
-
-from klgists import logger
+from typing import Iterable, Mapping, Callable, Optional
+from enum import Enum
 from colorama import Fore, Style
 
-def warn_user(*lines: str):
-	print()
-	warn_thin(*lines)
-	print()
-def warn_thin(*lines: str):
-	print_to_user(['', *lines, ''], Fore.RED)
 
-def notify_user(*lines: str):
-	print()
-	notify_thin(*lines)
-	print()
-def notify_thin(*lines: str):
-	print_to_user(['', *lines, ''], Fore.BLUE)
+class NotificationLevel(Enum):
+	INFO = 1
+	SUCCESS = 2
+	NOTICE = 3
+	WARNING = 4
+	FAILURE = 5
 
-def success_to_user(*lines: str):
-	print()
-	success_to_user_thin(*lines)
-	print()
-def success_to_user_thin(*lines: str):
-	print_to_user(['', *lines, ''], Fore.GREEN)
 
-def concern_to_user(*lines: str):
-	print()
-	concern_to_user_thin(*lines)
-	print()
-def concern_to_user_thin(*lines: str):
-	print_to_user(['', *lines, ''], Fore.MAGENTA)
+class ColorMessages:
 
-def notify_user_light(*lines: str):
-	print()
-	notify_light_thin(*lines)
-	print()
-def notify_light_thin(*lines: str):
-	print_to_user(['', *lines, ''], Style.BRIGHT, sides='')
+	DEFAULT_COLOR_MAP = {
+		NotificationLevel.INFO: Style.BRIGHT,
+		NotificationLevel.NOTICE: Fore.BLUE,
+		NotificationLevel.SUCCESS: Fore.GREEN,
+		NotificationLevel.WARNING: Fore.MAGENTA,
+		NotificationLevel.FAILURE: Fore.RED
+	}
 
-def header_to_user(*lines: str):
-	print_to_user(lines, Style.BRIGHT)
+	def __init__(self, color_map: Optional[Mapping[NotificationLevel, int]] = None, log_fn: Optional[Callable[[str], None]] = None, **kwargs):
+		"""
+		Constructs a new environment for colored console messages.
+		:param color_map: A map from level to colors in colorama to override ColorMessages.DEFAULT_COLOR_MAP
+		:param log_fn: If set, additionally logs every message with this function
+		:param kwargs: Arguments 'top', 'bottom', 'sides', and 'line_length'
+		"""
+		_cmap = ColorMessages.DEFAULT_COLOR_MAP
+		_cmap.update(color_map)
+		assert set(_cmap.keys()) == set(NotificationLevel.__members__.keys()),\
+			"Color map {} must match levels {}".format(_cmap, NotificationLevel.__members__)
+		self._color_map, self._log_fn, self._kwargs = _cmap, log_fn, kwargs
 
-def print_to_user(lines: Iterable[str], color: int, top: str='_', bottom: str='_', sides: str='', line_length: int=100):
-	def cl(text: str): print(str(color) + sides + text.center(line_length - 2*len(sides)) + sides)
-	print(str(color) + top * line_length)
-	logger.debug(top * line_length)
-	for line in lines:
-		logger.debug(line)
-		cl(line)
-	print(str(color) + bottom * line_length)
-	logger.debug(bottom * line_length)
+	def thin(self, level: NotificationLevel, *lines: str):
+		self._print(lines, self._color_map[level], **self._kwargs)
 
+	def thick(self, level: NotificationLevel, *lines: str):
+		self._print(['\n', lines, '\n'], self._color_map[level], **self._kwargs)
+
+	def _print(self, lines: Iterable[str], color: int, top: str = '_', bottom: str = '_', sides: str = '', line_length: int = 100):
+		def cl(text: str): print(str(color) + sides + text.center(line_length - 2 * len(sides)) + sides)
+		print(str(color) + top * line_length)
+		self._log(top * line_length)
+		for line in lines:
+			self._log(line)
+			cl(line)
+		print(str(color) + bottom * line_length)
+		self._log(bottom * line_length)
+
+	def _log(self, message):
+		if self._log_fn:
+			self._log_fn(message)
+
+
+__all__ = ['ColorMessages']
