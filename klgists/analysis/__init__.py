bb1e68c32bc7fbb02c769c8092bf4cdb827ff13e
Chris Ki <chriski777@berkeley.edu>
2020-01-15 16:46:52 -0800

    Finished cleaning up klgists/analysis

--- klgists/analysis/__init__.py
+++ klgists/analysis/__init__.py
@@ -1,13 +1,2 @@
 import numpy as np
 
-
-def sliding_window(x: np.ndarray, n: int) -> np.ndarray:
-	"""Returns a sliding window of n elements from x.
-	Raises a ValueError of n > len(x).
-	"""
-	if n > len(x): raise ValueError("N must be less than the array length")
-	# Courtesy of https://stackoverflow.com/questions/13728392/moving-average-or-running-mean
-	return np.convolve(x, np.ones((n,)) / n, mode='valid')
-
-
-__all__ = ['sliding_window']
